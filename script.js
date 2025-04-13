6ebc17ed909a08b66ba4f9bbadff7a347314c0f9
murilomorano3 <133981366+murilomorano3@users.noreply.github.com>
2025-04-13 16:17:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ document.addEventListener('DOMContentLoaded', function(){
     botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === true;
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -23,4 +28,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     })
 
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
+
 })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
